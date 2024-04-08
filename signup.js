8761652ed9a6fb78c2220eeabf448e77206c5e85
AmalieThomas <Amalie.thomas@hotmail.dk>
2024-04-08 12:57:14 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,15 +1,11 @@
-
-
 function validatePassword() {
     var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
+    var confirm_password = document.getElementById("confirm_password").value;
 
-    if (password !== confirmPassword) {
-        document.getElementById("message").textContent = "Passwords do not match!";
-        return false;
-    } else {
-        document.getElementById("message").textContent = "Passwords match.";
-        // You can also add code here to submit the form
-        return true;
+    if (password !== confirm_password) {
+        document.getElementById("message").innerHTML = "Adgangskode er forkert"
+        return false; // Prevent form submission
     }
+
+    return true; // Allow form submission
 }
