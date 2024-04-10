9b7db5869e011dbd3ba37c122e51be5dc5707db6
AmalieThomas <Amalie.thomas@hotmail.dk>
2024-04-10 10:36:38 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -10,30 +10,6 @@ function validatePassword() {
     return true; // Allow form submission
 }
 
-function previewImage() {
-    
-}
-
- /* Function to display selected image in the preview
- function previewImage(event) {
-    // Get reference to the input element
-    var input = event.target;
-
-    var reader = new FileReader();
-
-        // Set the image once loaded into FileReader
-        reader.onload = function(e) {
-            // Get the loaded image and display it
-            var previewImage = document.getElementById('preview');
-            previewImage.src = e.target.result;
-        };
-
-        // Load image file into FileReader
-        reader.readAsDataURL(input.files[0]);
-    }
-    
-}*/
-
 let ProfilePic = document.getElementById("profile-pic")
 let inputFile = document.getElementById("input-file")
 
