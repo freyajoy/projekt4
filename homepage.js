4ea127d3ff1ded2bc136b926ec0cc6fe2699268b
Ceciliekknudsen <ceciliekfknudsen@gmail.com>
2024-04-11 14:27:48 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -4,11 +4,7 @@
 let pictures = [ 
     "/images/Wilma.png",
     "/images/sigurd.png",
-    "/images/Wilma.png",
-    "/images/sigurd.png",
-    "/images/Wilma.png",
-    "/images/sigurd.png"
-]
+    "/images/Wilma.png"]
 
 let container = document.getElementById('billedbeholder');
      
@@ -16,12 +12,40 @@ for (let i = 0; i < pictures.length; i++) {
     let img = document.createElement('img');
     img.src = pictures[i];
     container.appendChild(img);
+
 }
 
 let karrusel= document.querySelector(".karrusel")
-let  dragging = (e) => {
-    karrusel.scrollLeft = e.pageX || e.touches[0].pageX;
-}
+
+let swipe=false
+
+karrusel.addEventListener("mousedown", dragStart);
+karrusel.addEventListener("touchstart", dragStart);
 
 karrusel.addEventListener("mousemove", dragging);
 karrusel.addEventListener("touchmove", dragging);
+
+karrusel.addEventListener("mouseup", dragStop);
+karrusel.addEventListener("mouseleave", dragStop);
+karrusel.addEventListener("touchend", dragStop);
+
+
+
+function dragStart (e) { 
+    karrusel.scrollLeft = e.pageX 
+    swipe=true
+    e.preventDefault()  ;}
+
+function dragging (e) { 
+    karrusel.scrollLeft = e.pageX;
+    swipe=false
+    e.preventDefault()}
+
+function dragStop (e) { 
+    karrusel.scrollLeft = e.pageX;
+    e.preventDefault()}
+
+
+//|| mousemove.scrollLeft[0].pageX
+
+
