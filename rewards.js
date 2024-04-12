6bde6c59b9ef9a1768228e26df5727e2a6c35da0
nayacatling <nayacatling12@gmail.com>
2024-04-12 11:54:40 +0200

    Update rewards.js

--- rewards.js
+++ rewards.js
@@ -87,3 +87,34 @@ function createDots() {
 
 // Når DOM'en er fuldt indlæst, kald createDots for at initialisere dots.
 document.addEventListener('DOMContentLoaded', createDots);
+
+// Pop up
+let popup = document.getElementById("åben");
+
+function åben() { 
+    popup.style.display="block"
+         document.getElementById("rabatk").innerHTML = kode;
+}
+
+function luk() {
+    popup.style.display="none";
+}
+
+hour = 14;
+
+let rabatkoder = ['GA12EF', 'A394ED', 'B5893F'];
+let kode = "";
+
+for (var i =0; i<rabatkoder.length; ++i) {
+    kode +=  rabatkoder[i] 
+}
+
+if (hour <12) { kode = "<p>" + rabatkoder[0] + "</p>"
+}
+
+else if (hour==12){ kode = "<p>" + rabatkoder[1] + "</p>" }
+
+else {
+
+    kode = "<p>" + rabatkoder[2] + "</p>"
+}
